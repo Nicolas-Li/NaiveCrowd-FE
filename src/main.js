67b43ca89e1fc7413c39f234dfdbf17a26cf2268
李俊杰 <2018011426@secoder.net>
2020-10-09 15:35:31 +0800

    install vue-cookies

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuex from 'vuex'
-import VueRouter from 'vue-router'
 // 完整引入 Element
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 // 引入router
+import VueRouter from 'vue-router'
 import router from './router/router'
+// 引入vue-cookies
+import VueCookies from 'vue-cookies'
 
 Vue.config.productionTip = false
 Vue.use(Vuex)
+Vue.use(ElementUI)
 Vue.use(VueRouter)
-Vue.use(ElementUI);
+Vue.use(VueCookies)
 
 new Vue({
   router,
