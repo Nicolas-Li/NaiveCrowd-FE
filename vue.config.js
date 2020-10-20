133fa353515d219649ea6e49cec0a7031d4fcf88
李俊杰 <2018011426@secoder.net>
2020-10-20 15:00:46 +0800

    before week6 start

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://naivecrowdbackend-81x86.app.secoder.net:80',
-                // target: 'http://localhost:8000',
+                // target: 'http://naivecrowdbackend-81x86.app.secoder.net:80',
+                target: 'http://localhost:8000',
                 // target: 'http://183.172.122.157:8000',
                 ws: true,
                 changeOrigin: true
