838f82c4fb9fdfcdbaf303ec676fb027c7537eb1
Nipers <xylu18@gmail.com>
2020-11-04 03:24:35 +0800

    写了很少一部分测试

--- __tests__/demander/task.spec.js
+++ __tests__/demander/task.spec.js
@@ -14,6 +14,55 @@ describe('Task',()=>{
     it('have right buttons',()=>{
         expect(wrapper.findAll('.el-button').length).toBe(1)
     })
+    it('can show button-content rightly', ()=> {
+        expect(button.text()).toBe('配置任务')
+    })
+    it('can change button-content rightly',async ()=> {
+        await wrapper.findAll('.el-button').at(0).trigger('click')
+        expect(button.text()).toBe('进入配置')
+    })
+    it('can show percentage rightly', async () =>{
+        await wrapper.setProps({
+            task:{
+                id: "3a4d567",
+                title: "标题",
+                intro: "介绍",
+                status: 2,
+                time: new Date().getTime()
+            }
+        })
+        await wrapper.findAll('.el-button').at(0).trigger('click')
+        expect (wrapper.vm.task.status).toBe(2)
+        expect (wrapper.findAll('.el-progress').length).toBe(1)
+    })
+    it('can check demander money rightly',async ()=> {
+        await wrapper.setProps({
+            task:{
+                id: "3a4d567",
+                title: "标题",
+                intro: "介绍",
+                status: 1,
+                time: new Date().getTime()
+            }
+        })
+        await wrapper.findAll('.el-button').at(0).trigger('click')
+    })
+
+    it('can break',async ()=> {
+        await wrapper.setProps(
+            {
+                task:{
+                    id: "3a4d567",
+                    title: "标题",
+                    intro: "介绍",
+                    status: 7,
+                    time: new Date().getTime()
+                }
+            }
+        )
+        await wrapper.findAll('.el-button').at(0).trigger('click')
+        expect (wrapper.vm.task.status).toBe(7)
+    })
 
     //wrapper.setData({loading:true,money:100})
 
