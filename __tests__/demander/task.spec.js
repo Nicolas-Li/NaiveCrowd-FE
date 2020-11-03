baf52e094c899674e2cf210d0566450f551b117b
Nipers <xylu18@gmail.com>
2020-11-03 21:36:01 +0000

    ready to merge yxb

--- __tests__/demander/task.spec.js
+++ __tests__/demander/task.spec.js
@@ -8,8 +8,8 @@ config.stubs.transition = false
 
 
 describe('Task',()=>{
-    const wrapper = mount(Task,{localVue})
-    const button=wrapper.find('.el-button')
+    var wrapper = mount(Task,{localVue})
+    var button=wrapper.find('.el-button')
 
     it('have right buttons',()=>{
         expect(wrapper.findAll('.el-button').length).toBe(1)
@@ -21,63 +21,60 @@ describe('Task',()=>{
         await wrapper.findAll('.el-button').at(0).trigger('click')
         expect(button.text()).toBe('进入配置')
     })
+    wrapper.destroy()
+    wrapper = mount(Task,{
+        task:{
+            id: "3a4d567",
+            title: "标题",
+            intro: "介绍",
+            status: 2,
+            time: new Date().getTime()
+        }
+    })
     it('can show percentage rightly', async () =>{
-        await wrapper.setProps({
-            task:{
-                id: "3a4d567",
-                title: "标题",
-                intro: "介绍",
-                status: 2,
-                time: new Date().getTime()
-            }
-        })
         wrapper.findAll('.el-button').at(0).trigger('click')
         expect (wrapper.vm.task.status).toBe(2)
         expect (wrapper.findAll('.el-progress').length).toBe(1)
         expect (wrapper.vm.loading).toBe(true);
     })
+    wrapper.destroy()
+    wrapper = mount(Task,{
+        task:{
+            id: "3a4d567",
+            title: "标题",
+            intro: "介绍",
+            status: 1,
+            time: new Date().getTime()
+        }
+    })
     it('can check demander money rightly',async ()=> {
-        await wrapper.setProps({
-            task:{
-                id: "3a4d567",
-                title: "标题",
-                intro: "介绍",
-                status: 1,
-                time: new Date().getTime()
-            }
-        })
         await wrapper.findAll('.el-button').at(0).trigger('click')
     })
-
+    wrapper.destroy()
+    wrapper = mount(Task,{
+        task:{
+            id: "3a4d567",
+            title: "标题",
+            intro: "介绍",
+            status: 7,
+            time: new Date().getTime()
+        }
+    })
     it('can break',async ()=> {
-        await wrapper.setProps(
-            {
-                task:{
-                    id: "3a4d567",
-                    title: "标题",
-                    intro: "介绍",
-                    status: 7,
-                    time: new Date().getTime()
-                }
-            }
-        )
-        await wrapper.findAll('.el-button').at(0).trigger('click')
+        await wrapper.find('.el-button').trigger('click')
         expect (wrapper.vm.task.status).toBe(7)
     })
-
+    wrapper = mount(Task,{
+        task:{
+            id: "3a4d567",
+            title: "标题",
+            intro: "介绍",
+            status: 3,
+            time: new Date().getTime()
+        }
+    })
     it('can finish',async ()=> {
-        await wrapper.setProps(
-            {
-                task:{
-                    id: "3a4d567",
-                    title: "标题",
-                    intro: "介绍",
-                    status: 3,
-                    time: new Date().getTime()
-                }
-            }
-        )
-        wrapper.findAll('.el-button').at(0).trigger('click')
+        wrapper.find('.el-button').trigger('click')
         expect (wrapper.vm.task.status).toBe(3)
     })
 
