037e4ab899dc18d6317ebef585cf2d839fdc1769
于小斌 <yuxiaobin18thu@163.com>
2020-11-04 23:19:24 +0800

    #22

---
+++ __tests__/demander/acceptance.spec.js
@@ -0,0 +1,37 @@
+import {shallowMount,mount,config,createLocalVue} from '@vue/test-utils'
+import Acceptance from '@/components/task/demander/acceptance/Acceptance'
+import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
+import VueCookies from 'vue-cookies'
+//import router from '../../src/router/router.js'
+import Vue from 'vue'
+const localVue=createLocalVue()
+localVue.use(ElementUI)
+localVue.use(VueCookies)
+config.stubs.transition = false
+
+
+describe('Acceptance',()=>{
+    const wrapper = shallowMount(Acceptance,{
+        localVue,
+        mocks:{
+            $route:{
+                params:{id:'1',status:0}
+            },
+        }
+    })
+
+    //wrapper.setData({loading:true,money:100})
+
+    it('money',async()=>{
+
+        wrapper.vm.assertTaskId()
+        wrapper.vm.seeAnswersOfUsers(1,1)
+        wrapper.vm.showAnswer()
+        wrapper.vm.hideAnswer()
+        wrapper.vm.seeAnswersOfUser(answerId)
+        wrapper.vm.normalIndex(3,1)
+    })
+
+    
+})
