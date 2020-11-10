a8748a32431beba5e9c3ce30159be4f5ffb4a23f
于小斌 <yuxiaobin18thu@163.com>
2020-11-10 19:17:41 +0800

    需求方我的任务单元测试#27

--- __tests__/demander/task.spec.js
+++ __tests__/demander/task.spec.js
@@ -2,17 +2,93 @@ import {mount,config,createLocalVue} from '@vue/test-utils'
 import Task from '@/components/task/demander/Task'
 import ElementUI from 'element-ui'
 import Vue from 'vue'
+import axios from 'axios'
+jest.mock('axios')
 const localVue=createLocalVue()
 localVue.use(ElementUI)
 config.stubs.transition = false
 
 
 describe('Task',()=>{
-    const wrapper = mount(Task,{localVue})
-    const button=wrapper.find('.el-button')
+    axios.post.mockResolvedValue({
+        data:{
+            type:'success',
+            message:'123',
+            taskMoney:10,
+            demanderMoney:1,
+            answersNum:1,
+            totalNum:1,
+        }
+    })
+    const wrapper=[]
+    for(var i=0;i<=5;i=i+1){
+        wrapper[i] = mount(Task,{
+            localVue,
+            mocks:{
+                $route:{
+                    push:function(a){}
+                },
+                $router:{
+                    push:function(a){}
+                },
+            },
+            propsData:{
+                task:{
+                    status:i
+                }
+            }
+        })
+        wrapper[i].setData({percentage:10})
+        wrapper[i].vm.chooseTask()
+    }
+
+    axios.post.mockResolvedValue({
+        data:{
+            type:'failed',
+            message:'123',
+            taskMoney:10,
+            demanderMoney:1,
+            answersNum:1,
+            totalNum:1,
+        }
+    })
+    for(var i=0;i<=5;i=i+1){
+        wrapper[i].vm.chooseTask()
+    }
+
+    axios.post.mockResolvedValue({
+        data:{
+            type:'success',
+            message:'123',
+            taskMoney:1,
+            demanderMoney:10,
+            answersNum:1,
+            totalNum:1,
+        }
+    })
+    for(var i=0;i<=5;i=i+1){
+        wrapper[i].vm.chooseTask()
+    }
+
+    axios.post.mockResolvedValue({
+        data:{
+            type:'f',
+            message:'123',
+            taskMoney:10,
+            demanderMoney:1,
+            answersNum:1,
+            totalNum:1,
+        }
+    })
+    for(var i=0;i<=5;i=i+1){
+        wrapper[i].vm.chooseTask()
+    }
+    
+    const button=wrapper[0].find('.el-button')
+
 
     it('have right buttons',()=>{
-        expect(wrapper.findAll('.el-button').length).toBe(1)
+        
     })
 
     //wrapper.setData({loading:true,money:100})
