b4511c82e1c35afd91d4afdc2a2a079556f20ea0
于小斌 <yuxiaobin18thu@163.com>
2020-11-10 19:31:16 +0800

    taskview test#30

---
+++ __tests__/user/taskView.spec.js
@@ -0,0 +1,50 @@
+import {shallowMount,mount,config,createLocalVue} from '@vue/test-utils'
+import TaskView from '@/components/task/user/TaskView'
+import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
+import VueCookies from 'vue-cookies'
+import Vue from 'vue'
+import axios from 'axios'
+const localVue=createLocalVue()
+localVue.use(ElementUI)
+localVue.use(VueCookies)
+config.stubs.transition = false
+jest.mock('axios')
+
+describe('TaskView',()=>{
+    const wrapper = shallowMount(TaskView,{
+        localVue,
+        mocks:{
+            $router:{
+                push:function(path){},
+                back:function(){}
+            },
+            $route:{
+                params:{
+                    id:1,
+                    status:0
+                }
+            }
+        }
+    })
+    const wrapper1 = shallowMount(TaskView,{
+        localVue,
+        mocks:{
+            $router:{
+                push:function(path){},
+                back:function(){}
+            },
+            $route:{
+                params:{
+                    id:1,
+                    status:1
+                }
+            }
+        }
+    })
+
+    it('m',async()=>{
+        wrapper.vm.doTask()
+    })
+    
+})
