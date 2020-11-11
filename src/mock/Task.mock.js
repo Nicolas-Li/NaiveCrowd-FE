568e4fe3ebccf6c0baa0c27c4ef4462fb2327224
于小斌 <yuxiaobin18thu@163.com>
2020-11-11 15:36:17 +0800

    add collect task 35

--- src/mock/Task.mock.js
+++ src/mock/Task.mock.js
@@ -4,20 +4,24 @@ import API from "@/net/API"
 Mock.setup({timeout: "200-400"})
 
 let taskList = []
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 50; i++) {
     taskList.push({
         id: 1,
         cover: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
         title: "任务样例",
-        intro: "介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍",
+        intro: "介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍",
         time: new Date().getTime(),
         questionNum: 10000,
         miniTaskNum: 1000,
-        status: "已发布"
+        status: 1
     })
 }
 Mock.mock(API.LOAD_MORE_TASK.path,API.LOAD_MORE_TASK.method,(rqst)=>{
     let data = JSON.parse(rqst.body)
+    for (let i = 0; i < taskList.length; i++) {
+        taskList[i].status = i % 6
+        taskList[i].id = i + 1
+    }
     return { data: taskList.slice(data.start, data.start + data.num) }
 })
 Mock.mock(API.LOAD_TASK.path,API.LOAD_TASK.method,(rqst)=>{
@@ -26,4 +30,15 @@ Mock.mock(API.LOAD_TASK.path,API.LOAD_TASK.method,(rqst)=>{
         taskList[i].id = i + 1
     }
     return { data: taskList.slice(0, 10) }
+})
+Mock.mock(API.LOAD_ALL_TASK.path,API.LOAD_ALL_TASK.method,(rqst)=>{
+    for (let i = 0; i < taskList.length; i++) {
+        taskList[i].status = i % 6
+        taskList[i].id = i + 1
+        taskList[i].title="任务样例"+String(i)
+    }
+    return { data: taskList.slice(0, 100) }
+})
+Mock.mock(API.GET_FAVOR.path,API.GET_FAVOR.method,(rqst)=>{
+    return { data: [3] }
 })
