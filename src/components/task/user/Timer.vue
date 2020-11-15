922d230caa0705667e5857b4e082e03dd84cceaa
于小斌 <yuxiaobin18thu@163.com>
2020-11-15 22:33:54 +0800

    增加计时器Timer组件#50

---
+++ src/components/task/user/Timer.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="timer">
+        <div ref="startTimer">00:00:00</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Timer',
+    props:{
+      time:{
+        type:Number
+      }
+    },
+    data() {
+        return {
+            timer: "",
+            hour: 0,
+            minutes: 0,
+            seconds: 0,
+            cr: ''
+        }
+    },
+    created() {
+        this.timer = setInterval(this.startTimer, 1000);
+    },
+    methods: {
+        startTimer() {
+            this.seconds += 1;
+            if (this.seconds >= 60) {
+                this.seconds = 0;
+                this.minutes = this.minutes + 1;
+            }
+
+            if (this.minutes >= 6) {
+                this.minutes = 0;
+                this.hour = this.hour + 1;
+            }
+            this.$refs.startTimer.innerHTML = (this.hour < 10 ? '0' + this.hour: this.hour) + ':' + (this.minutes < 10 ? '0' + this.minutes: this.minutes) + ':' + (this.seconds < 10 ? '0' + this.seconds: this.seconds);
+            this.cr =  this.seconds
+        },
+        stop () {
+            clearInterval(this.timer)
+        },
+        start () {
+            this.timer = setInterval(this.startTimer, 1000)
+        }
+    }
+}
+</script>
