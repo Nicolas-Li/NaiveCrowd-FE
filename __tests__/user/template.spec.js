a144114a3d7f26033ef441ac5af4d3057a2a6578
于小斌 <yuxiaobin18thu@163.com>
2020-11-18 21:32:27 +0800

    单元测试bug：TypeError: Cannot read property 'start' of undefined #56

--- __tests__/user/template.spec.js
+++ __tests__/user/template.spec.js
@@ -23,17 +23,11 @@ describe('template',()=>{
                 query:{
                     task: 0
                 }
-            }
+            },
         }
     })
     it('has right id', ()=> {
         expect(wrapper.vm.currentPage).toBe(1)
-    })
-    const wrapper2 = mount(DoTaskTemplate, {localVue,})
-    const buttons = wrapper2.findAll('.el-button')
-    it('has right button', async()=>{
-        expect(buttons.length).toBe(1)
-        await buttons.at(0).trigger('click')
     })
 
 })
