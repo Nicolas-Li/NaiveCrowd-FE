37b4144c1dc714b7619f03574dc8cdf04e3cab2e
于小斌 <yuxiaobin18thu@163.com>
2020-11-18 22:18:31 +0800

    #57 计时器组件  Timer.vue的单元测试

---
+++ __tests__/user/timer.spec.js
@@ -0,0 +1,44 @@
+import {shallowMount,mount,config,createLocalVue} from '@vue/test-utils'
+import Timer from '@/components/task/user/Timer'
+import ElementUI from 'element-ui'
+import Vue from 'vue'
+const localVue=createLocalVue()
+localVue.use(ElementUI)
+config.stubs.transition = false
+jest.mock('axios')
+
+describe('timer',()=>{
+    const wrapper = shallowMount(Timer,{
+        localVue,
+        propsData: {
+            maxtime:8000
+        },
+        mocks:{
+            $router:{
+                push:function(path){},
+                back:function(){}
+            },
+            $route:{
+                params:{
+                    id:1,
+                    status:0
+                },
+                query:{
+                    task: 0
+                }
+            },
+        }
+    })
+    it('has right id', ()=> {
+        wrapper.vm.startTimer()
+        wrapper.setData({minutes: 1000,
+            seconds: 100,})
+        wrapper.vm.startTimer()
+        wrapper.setData({minutes: 61,
+            seconds: 100,})
+        wrapper.vm.startTimer()
+        wrapper.vm.stop()
+        wrapper.vm.start()
+    })
+
+})
