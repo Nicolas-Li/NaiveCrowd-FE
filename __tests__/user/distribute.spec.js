68e87f695507bb61a14ea867795583c1efeb03b5
李俊杰 <2018011426@secoder.net>
2020-11-25 15:50:48 +0800

    修复系统分发的单元测试showDate的bug #74

--- __tests__/user/distribute.spec.js
+++ __tests__/user/distribute.spec.js
@@ -77,7 +77,7 @@ describe('Distribute',()=>{
         wrapper.vm.getTask()
         wrapper.vm.getMiniTaskByTask()
         wrapper.vm.submitAnswer('')
-        wrapper.vm.showDate()
+        wrapper.vm.showDate
     })
     
 })
